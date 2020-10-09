c63572bb06e0ef8be29dba95dce7a14333954d27
David Hutchison <dhutchis68516@gmail.com>
2020-10-09 16:20:12 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -10,9 +10,12 @@ class Show <ActiveRecord::Base
   end
 
   def self.lowest_rating
+    Show.minnimum(:rating)
   end
 
   def self.least_popular_show
+    show = Show.find_by(rating: Show.highest_rating)
+    show
   end
 
   def self.ratings_sum
