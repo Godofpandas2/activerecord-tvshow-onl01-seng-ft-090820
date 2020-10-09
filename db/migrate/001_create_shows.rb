c35d4b37e76a539ac00cd3da4e8b6edee67f3be2
David Hutchison <dhutchis68516@gmail.com>
2020-10-09 16:07:19 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -3,7 +3,7 @@ class CreateShows < ActiveRecord::Migration[5.2]
     create_table :shows do |x|
       x.string :name
       x.string :network
-      x.integer :day
+      x.string :day
       x.integer :rating
     end
   end
